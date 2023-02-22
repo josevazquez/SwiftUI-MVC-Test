46b1e8c2be05156eaebe10502afebe6bee1806a8
Jose Vazquez <jose@teguz.com>
2023-02-22 15:30:38 -0600

    Initial Commit

---
+++ SwiftUI MVC Test/SwiftUI_MVC_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVC_TestApp.swift
+//  SwiftUI MVC Test
+//
+//  Created by Jose Vazquez on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVC_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
